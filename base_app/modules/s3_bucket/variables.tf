95aa82a6c07ec83156e7df227c29f2ccd42ee8d7
ned1313 <ned.bellavance@gmail.com>
2025-08-12 10:04:21 -0400

    remove comments in base app

--- base_app/modules/s3_bucket/variables.tf
+++ base_app/modules/s3_bucket/variables.tf
@@ -1,12 +1,3 @@
-/*
-
-You need to add new variables to the s3_bucket module to configure the following:
-
-Add a variable for tags to be applied to the bucket.
-
-
-*/
-
 variable "bucket_prefix" {
   description = "Prefix for S3 bucket name"
   type        = string
